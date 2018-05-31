c79e9b1feb9d4778d2eb18f4a98c8180947360e6
rameshvjayakody <rameshvjayakody@gmail.com>
2018-05-31 21:09:18 +0530

    This is a test code to commit...

---
+++ src/javaapplication326/JavaApplication326.java
@@ -0,0 +1,8 @@
+package javaapplication326;
+public class JavaApplication326 {
+    public static void main(String[] args) {
+        String str="Hello Ramesh";
+        float i=5.6f;
+        System.out.println(Float.toString(i));
+    }
+}
